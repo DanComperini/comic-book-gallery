ac046cd0d7e280d01fe2e65bf389faa1bb98bb3f
dcomperini <dcomperini@GBO-DCOMPHP>
2017-03-19 19:59:30 -0500

    Updated comic book detail view to be strongly typed.

--- src/ComicBookGallery/Models/ComicBook.cs
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -8,7 +8,6 @@ namespace ComicBookGallery.Models
 {
     public class ComicBook
     {
-
         public int Id { get; set; }
         public string SeriesTitle { get; set; }
         public int IssueNumber { get; set; }
